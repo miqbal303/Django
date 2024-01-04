bcd925d9cb4c20873af8947d18b0a95dd2a5f365
M Iqbal <miqbal303@gmail.com>
2024-01-04 23:26:04 +0530

    render templates

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
+def myfunctioncall(request):
+    return HttpResponse("Hello")
+
+def myfunctionabout(request):
+    return HttpResponse("About Response")
+
+def add(request, a, b):
+    return HttpResponse(a+b)
+
+def intro(request, name, age):
+    dict = {"name":name,
+            "age": age}
+    return JsonResponse(dict)
+
+def myfirstpage(request):
+    return render(request, "index.html")
+
+def mysecondpage(request):
+    return render(request, "second.html")
